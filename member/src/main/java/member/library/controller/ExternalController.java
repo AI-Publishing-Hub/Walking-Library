3c799430d01f074b3113fd7e443f392a33d3fe4d
KIMMINSEOK <sanmaru3u@naver.com>
2025-07-02 20:09:30 +0900

    member route modified

--- member/src/main/java/member/library/controller/ExternalController.java
+++ member/src/main/java/member/library/controller/ExternalController.java
@@ -11,7 +11,7 @@ import member.library.domain.UserRepository;
 import member.library.domain.User.SubscriptionStatus;
 import member.library.domain.User.role;
 @RestController
-@RequestMapping("/api/members")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class ExternalController {
 
